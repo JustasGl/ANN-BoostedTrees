5a99cfcb41bdf4ea88fda7e5a3bce016f90614d5
JustasGl <35509569+JustasGl@users.noreply.github.com>
2021-06-05 16:09:41 +0300

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -9,17 +9,30 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')
 
 df.pop('id')
-df = df.dropna()
+df = df.dropna()#we marged all 5 years in one data file. And in data there were a lot of empty values were marked 
+#with question mark sign Mantas converted them to null values and then we droped null values from dataframe
 
-df1 = df.loc[df['class'] == 1]
-df2 = df.loc[df['class'] == 0][:int(len(df1)*1)] 
+testing = df[0:int(len(df)*0.2)]
+training = df[int(len(df)*0.2):]
 
-df = pd.concat([df1,df2])
+df1 = training.loc[training['class'] == 1]
+df2 = training.loc[training['class'] == 0][:int(len(df1)*1)] #because our data had inbalanced data. 
+#Out of 43 thousand data lines there where 95 pecent of nonbankrupt companies and only 5 percent bancrupted how awfull is that right? Only around 5 % companies bancrupted.
+# So with that in mind we searched for posible solutions. One of them was downsampling. SO we applied it. 
+# #After downsampling there was 50/50 data distributions between bancrupt and non bancrupt companies
 
-target = df.pop('class')
-df = df.to_numpy()
-target = target.to_numpy()
-xtrain, xtest,ytrain ,ytest = train_test_split(df, target, test_size = 0.2, random_state = 42)
+training = pd.concat([df1,df2])
+
+Testtarget = testing.pop('class')
+TrainTarget = training.pop('class')
+
+testing = testing.to_numpy()
+training = training.to_numpy()
+
+Testtarget = Testtarget.to_numpy()
+TrainTarget = TrainTarget.to_numpy()
+
+training, testing,TrainTarget ,Testtarget = train_test_split(df, target, test_size = 0.2, random_state = 42)
 
 
 model = tf.keras.models.Sequential([
@@ -32,15 +45,15 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(xtrain, ytrain, epochs=25)
+model.fit(training, TrainTarget, epochs=25)
 
-test_loss, test_acc = model.evaluate(xtest, ytest, verbose=2)
+test_loss, test_acc = model.evaluate(testing, Testtarget, verbose=2)
 
-a = np.argmax(model.predict(xtest), axis=-1)
+a = np.argmax(model.predict(testing), axis=-1)
 
 print(model.summary())
 
-con_mat = tf.math.confusion_matrix(labels=ytest, predictions=a).numpy()
+con_mat = tf.math.confusion_matrix(labels=Testtarget, predictions=a).numpy()
 
 print(con_mat)
 
